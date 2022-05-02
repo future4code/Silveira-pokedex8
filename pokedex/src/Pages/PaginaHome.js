a9245919ba2c76e02b8cbf33b25b674a255105df
EduardaPmeireles <contatoeduardapmeireles@hotmail.com>
2022-05-02 17:04:09 -0300

    Adicionando esqueleto da página

---
+++ pokedex/src/Pages/PaginaHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PaginaHome = () => {
+
+    return(
+        <div>
+            página home
+        </div>
+    )
+}
+
+export default PaginaHome;
---
+++ pokedex/src/Pages/PaginaPokedex.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PaginaPokedex = () => {
+
+    return(
+        <div> Página pokedéx </div>
+    )
+}
+
+export default PaginaPokedex;
